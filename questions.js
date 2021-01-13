c2b2079921c4ed6401ce1c4f4698dc44cb2e9561
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-13 02:16:59 -0600

    highscores html, timer placement, css update to colors

---
+++ questions.js
@@ -0,0 +1,14 @@
+<!---Questions & Buttons-->
+<div class="col-md-6">
+    <div class="card">
+        <div class="card-body">
+            <h5 id="questions" class="card-title">What is HTML known as?
+            </h5>
+            <button id="answer1" type="button" class="btn btn-primary d-block mb-1">The appearance</button>
+            <button id="answer2" type="button" class="btn btn-primary d-block mb-1">The brain</button>
+            <button id="answer3" type="button" class="btn btn-primary d-block mb-1">The body</button>
+            <button id="answer4" type="button"
+                class="btn btn-primary d-block mb-1">How.The.Mind.Lives</button>
+        </div>
+    </div>
+</div>
