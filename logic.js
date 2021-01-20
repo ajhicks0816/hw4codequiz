eac6442ba2c913a48111c51b2e69d495e429d2f3
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-19 22:23:14 -0600

    scrapped everything, starting over, working on start button and showing of quiz questions

--- logic.js
+++ logic.js
@@ -35,57 +35,72 @@ var time = questions.length * 10;
 var timerId;
 
 //Variables
-var questionsEl = document.createElement("questions");
-var questionsTitle = document.getElementById("question-title");
-var timerEl = document.createElement("time");
-var choices = document.createElement("choices")
-var submitEl = document.createElement("submit")
-var startBtn = document.createElement("start");
-var intialsEl = document.createElement("initials")
+// var questionsEl = document.createElement("questions");
+// var questionsTitle = document.getElementById("question-title");
+// var timerEl = document.createElement("time");
+// var choices = document.createElement("choices")
+// var submitEl = document.createElement("submit")
+// var intialsEl = document.createElement("initials")
+var startBtn = document.getElementById("start");
+var currentQuestionDiv = document.getElementById("current-question");
+var choice1Btn = document.getElementById("choice-1");
+var choice2Btn = document.getElementById("choice-2");
+var choice3Btn = document.getElementById("choice-3");
+var choice4Btn = document.getElementById("choice-4");
+var currentQuestion = questions[0];
+// {
+//     title: "Commonly used date types DO NOT include:",
+//     choices: ["string", "booleans", "alerts", "numbers"],
+//     answer: "alerts"
+// }
+
+// // Start Quiz
+// var startScreenEL = document.getElementById("start-screen");
+// startScreenEL.setAttribute("class", "hide")
+startBtn.addEventListener("click", function () {
+    // console.log("clicked on the button");
+    // console.log(currentQuestion);
+    var questionTitle = currentQuestion.title;
+    currentQuestionDiv.textContent = questionTitle;
+    
+    var choices = currentQuestion.choices;
+    var choice1 = choices[0];
+    var choice2 = choices[1];
+    var choice3 = choices[2];
+    var choice4 = choices[3];
+    console.log(choice1);
+
+    choice1Btn.textContent = choice1;
+    choice2Btn.textContent = choice2;
+    choice3Btn.textContent = choice3;
+    choice4Btn.textContent = choice4;
+    // questionsEl.removeAttribute("class");
+
+    // timerId = setInterval(tickTock, 1000);
+
+    // timerEl.textContent = time;
+
+    // getQuestions();
+});
 
 
-// Start Quiz
-var startScreenEL = document.getElementById("start-screen");
-startScreenEL.setAttribute("class", "hide")
-startScreenEL.addEventListener("click", function () {
-    console.log("clicked on the button")
+// //Timer
+// function countdown() {
+//     var timeLeft = 15;
 
-    // console.log(questions[0].title)
-    // questionsTitle.textContent = questions[0].title
+//     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//     var timeInterval = setInterval(function () {
+//         console.log(timeLeft);
+//         count--;
+//         if (count === 0) {
+//             stopIntervel()
+//         }
+//     }, 1000);
 
-    questionsEl.removeAttribute("class");
-
-    timerId = setInterval(tickTock, 1000);
-
-    timerEl.textContent = time;
-
-    getQuestions();
-}
-
-function getQuestion() {
-
-    }
-
-
-);
-
-//Timer
-function countdown() {
-    var timeLeft = 15;
-
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-        console.log(timeLeft);
-        count--;
-        if (count === 0) {
-            stopIntervel()
-        }
-    }, 1000);
-
-    var stopInterval = function() {
-        console.log("time is up!");
-        clearInterval(timer);
-    }
+//     var stopInterval = function() {
+//         console.log("time is up!");
+//         clearInterval(timer);
+//     }
 
     // if (timeLeft === 1) {
     // timerEl.innerHTML = (timeLeft--) + " second left";
@@ -99,16 +114,13 @@ function countdown() {
     // timerEl.innerHTML = "";
     // clearInterval(timeInterval);
     // console.log("IF ELSE");
-    // displayMessage();
-}
-    // });
+//     // displayMessage();
+// }
+//     // });
 
 
-
-
-function displayTimer() {
-    console.log("Hello")
-}
+// function displayTimer() {
+//     console.log("Hello")
+// }
 
     // Click events
-};
